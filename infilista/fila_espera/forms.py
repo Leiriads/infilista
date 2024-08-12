c947d4884102f8f84537135258a5dcfc849578bf
Gabriel Leiria <89768557+Leiriads@users.noreply.github.com>
2024-08-12 05:08:07 -0300

    Delete infilista directory

--- infilista/fila_espera/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Crianca
-
-class CriancaForm(forms.ModelForm):
-    class Meta:
-        model = Crianca
-        fields = ['nome', 'idade', 'responsavel', 'telefone']
